8a877289bd9abac2533f0b379aba35bfa1b8be9c
Lukasz Bartkowiak <lubartkowiak@gmail.comlubartkowiak@gmail.comlubartkowiak@gmail.com>
2021-12-04 23:10:28 +0100

    Revert "Implement cookies page POM tests"
    This reverts commit 5e2bbf1808537bd8b044379dd6e3a3f2477e87a7.

--- POM/cookiesPage.js
+++ POM/cookiesPage.js
@@ -3,29 +3,17 @@ const { expect } = require('@playwright/test');
 
 exports.CookiesPage = class CookiesPage {
 
-    /**
-     * @param {import('@playwright/test').Page} page
-     */
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
     constructor(page) {
         this.page = page;
-        this.navigationMenu = page.locator('.submenu__content li')
-        this.emails = page.locator('a[href^=mailto]');
-
     }
 
-    async validatePageVisible() {
-
-    }
-
-    async validateNavigationMenu() {
-        await expect(this.navigationMenu.nth(4)).toHaveText('Contact');
+    async navigationMenuTitleIsCorrect() {     
     }
 
     async emailsAreCorrect() {
-        console.log(await this.emails);
-        for (let i = 0; i < await this.emails.count(); i++) {
-            await expect(this.emails.nth(i)).toContainText('@happn.fr');
-        }
     }
 
 }
--- POM/homePage.js
+++ POM/homePage.js
@@ -9,7 +9,6 @@ exports.HomePage = class HomePage {
     this.cookieButton = page.locator('button[aria-label="Accept cookies"]');
     this.aboutLink = page.locator('li.header__nav__item > a:text("About")');
     this.contactLink = page.locator('a.footer__main__link:text("Contact")');
-    this.cookiesLink = page.locator('a:has-text("Cookies")');
   }
 
   async navigate() {
@@ -26,7 +25,7 @@ exports.HomePage = class HomePage {
   }
 
   async navigateToCookies() {
-    await this.cookiesLink.click()
+
   }
 
   async navigateToContact() {
